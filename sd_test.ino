03819ac55fac74f3fdedbaf53a945d78b9d75dba
MikeCodes2586 <maikcodes@protonmail.com>
2022-06-13 16:57:23 +0200

    Add serial communication

--- sd_test.ino
+++ sd_test.ino
@@ -2,16 +2,35 @@
 
 #define PIN_SD_SS 4
 
-File file;
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
-void setup ()
-{
-	SD.begin( PIN_SD_SS );
-	SD.remove( "log.csv" );
-	file = SD.open ( "log.csv", FILE_WRITE );
-	if (file) 
-	{
-		file.write("Test");
-		file.close();
-	}
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(PIN_SD_SS)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+
+  SD.remove("log.csv");
+  
+  File file = SD.open("log.csv", FILE_WRITE);
+  Serial.println("file's there");
+
+  if (file)
+  {
+    file.println("Hi, Bitch!");
+    file.close();
+    Serial.println("Done!");
+  }
 }
+
+void loop() {}
